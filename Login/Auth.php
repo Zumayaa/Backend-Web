06f17eca8f27705421cfdd018ed865d8dae5633e
Zumayaa <abzu_21@alu.uabcs.mx>
2024-10-15 11:28:23 -0700

    login

--- Login/Auth.php
+++ Login/Auth.php
@@ -1,10 +1,29 @@
 <?php
     var_dump($_POST);
 
+    $authController = new AuthController();
+    $res = $authController->login($_POST["username"], $_POST["password"]);
+
     class AuthController
     {
         public function login($email='', $password)
         {
-            echo'Hola';
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('email' => $email,'password' => $password),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            return $response;
         }
     }
